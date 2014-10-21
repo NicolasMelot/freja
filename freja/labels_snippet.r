1869896267e4d78d3afc22ed317b918936c58fc6
Nicolas Melot <nicolas.melot@liu.se>
2014-10-21 17:15:48 +0200

    * Fixed issues with freja * Disabled logging by default * Upgraded label snippet to allow partial label declarations

--- freja/labels_snippet.r
+++ freja/labels_snippet.r
@@ -11,8 +11,23 @@ apply_labels = function(data.frame, labels.list = labels)
         data.frame[,col] <- as.factor(data.frame[,col])
       }
       
-      ## Reorder levels of factors in data.frame, and given them labels
-      data.frame[,col] <- factor(data.frame[,col], levels=names(labels.list[[col]]), labels=as.character(labels.list[[col]]))
+      ## Get levels and respective labels as from the label list
+      levels = names(labels.list[[col]])
+      labels = as.character(labels.list[[col]])
+      
+      ## Pad missing labels with values as they appear in data
+      if(length(levels(data.frame[,col])) > length(names(labels.list[[col]])))
+      {
+        ## Levels of data.frame, where levels that appear in labels appear first and ordered as in the list
+        levels = unique(c(names(labels.list[[col]]), levels(data.frame[,col])))
+        #levels = names(factor(data.frame[,col], levels=unique(c(labels.list[[col]], levels(data.frame[,col])))))
+        
+        ## Concatenate labels with Levels of data.frame that are not in the list of labels
+        labels = c(as.character(labels.list[[col]]), levels(factor(data.frame[,col][!data.frame[,col] %in% names(labels.list[[col]])])))
+      }
+
+      ## Reorder levels of factors in data.frame, and give them labels
+      data.frame[,col] <- factor(data.frame[,col], levels=levels, labels=labels)
       
       ## Only keep factors actually used in the table
       data.frame[,col] <- factor(data.frame[,col])
@@ -24,6 +39,9 @@ apply_labels = function(data.frame, labels.list = labels)
 
 label = function(col, columns="columns", labels.list = labels)
 {
-  return(labels.list[[columns]][col])
+  if(col %in% names(labels.list[[columns]]))
+    return(labels.list[[columns]][col])
+  else
+    return(col)
 }
 
