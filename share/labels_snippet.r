3967ec55aacf5fc935b0b4d3c6d2f57ebb23b19d
Nicolas Melot <nicolas.melot@liu.se>
2017-01-27 21:05:08 +0100

    * Updated label color vector generator for R

--- share/labels_snippet.r
+++ share/labels_snippet.r
@@ -29,7 +29,7 @@ append_combinations = function(data.frame, comb = combination)
     paste_str = paste(sep = "", paste_str, ")")
   
     eval(parse(text = paste_str))
-    data.frame[,col] <- as.factor(data.frame[,col])
+    data.frame[, col, drop=F] <- as.factor(data.frame[, col, drop=F])
   }
 
   return(data.frame)
@@ -46,9 +46,9 @@ apply_labels = function(data.frame, labels.list = labels, comb = combination)
     if(col %in% names(data.frame))
     {
       ## If the corresponding column in the data frame is not a factor, then make it one
-      if(!is.factor(data.frame[,col]))
+      if(!is.factor(data.frame[, col]))
       {
-        data.frame[,col] <- as.factor(data.frame[,col])
+        data.frame[, col] <- as.factor(data.frame[, col])
       }
       
       ## Get levels and respective labels as from the label list
@@ -56,20 +56,20 @@ apply_labels = function(data.frame, labels.list = labels, comb = combination)
       labels = as.character(labels.list[[col]])
       
       ## Pad missing labels with values as they appear in data
-      if(!all(levels(data.frame[,col]) %in% names(labels.list[[col]])))
+      if(!all(levels(data.frame[, col]) %in% names(labels.list[[col]])))
       {
         ## Levels of data.frame, where levels that appear in labels appear first and ordered as in the list
-        levels = unique(c(names(labels.list[[col]]), levels(data.frame[,col])))
+        levels = unique(c(names(labels.list[[col]]), levels(data.frame[, col])))
         
         ## Concatenate labels with Levels of data.frame that are not in the list of labels
-        labels = c(as.character(labels.list[[col]]), levels(factor(data.frame[,col][!data.frame[,col] %in% names(labels.list[[col]])])))
+        labels = c(as.character(labels.list[[col]]), levels(factor(data.frame[, col][!data.frame[, col] %in% names(labels.list[[col]])])))
       }
 
       ## Reorder levels of factors in data.frame, and give them labels
-      data.frame[,col] <- factor(data.frame[,col], levels=levels, labels=labels)
+      data.frame[, col] <- factor(data.frame[, col], levels=levels, labels=labels)
       
       ## Only keep factors actually used in the table
-      data.frame[,col] <- factor(data.frame[,col])
+      data.frame[, col] <- factor(data.frame[, col])
     }
   }
   
@@ -84,24 +84,30 @@ label = function(col, columns="columns", labels.list = labels)
     return(col)
 }
 
+display = function(x)
+{
+	print(x)
+	return(x)
+}
+
 freja_colors = function(data.frame, col, colors.list = colors, default = c("#BB0000", "#66BB00", "#0000BB"), comb = combination, sep = "_", inter=".")
 {
   ## Generate a vector of default colors
-  def = colorRampPalette(default)(abs(dim(unique(data.frame[,col]))[1] - length(colors[[paste(col, collapse = sep)]])))
+  def = colorRampPalette(default)(abs(nrow(unique(data.frame[, col, drop=F])) - length(colors[[paste(col, collapse = sep)]])))
   defindex=1
   ## Get the list of colors we are interested in
   colors = colors.list[[paste(col, collapse = sep)]]
  
   ## Keep only unique occurences of rows grouped by col 
-  data.frame = unique(data.frame[,col])
+  data.frame = unique(data.frame[, col, drop=F])
   ## Add a new column in data frame out of columns whose combined value gets one color
-  data.frame$color = apply(data.frame[,col] , 1, function(x) paste(trimws(x), collapse = "."))
+  data.frame$color = apply(data.frame[, col, drop=F] , 1, function(x) paste(gsub("^\\s", "", gsub("\\s$","", x, perl=T), perl=T), collapse = "."))
   ## Turn new coloumn to the corresponding color
   data.frame$color = unlist(lapply(data.frame[,"color"], function(x) {if( x %in% names(colors)) { return (colors[[x]])} else { col = def[[defindex]]; defindex <<- defindex + 1; return(col)}}))
   ## Replace content by reader-friendly content
   data.frame = apply_labels(data.frame)
   ## Associate reader-friendly content to corresponding color
-  data.frame$name = apply(data.frame[,col] , 1, function(x) paste(trimws(x), collapse = inter))
+  data.frame$name = apply(data.frame[, col, drop=F] , 1, function(x) paste(gsub("^\\s", "", gsub("\\s$","", x, perl=T), perl=T), collapse = inter))
   ## Keep only name and color
   data.frame = data.frame[,c("name", "color")]
   ## Form a list of colors from data frame
